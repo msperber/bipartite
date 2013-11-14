935f3d0009d100805b94a1a2aae5f0c572ff7677
Matthias Sperber <matthias.sperber@gmail.com>
2013-11-14 13:44:37 +0900

    integrated equation 15

--- bipartite-py/source/generative_algo.py
+++ bipartite-py/source/generative_algo.py
@@ -9,7 +9,7 @@ import math
 from source.prob import sampleFrom15
 import source.expressions as expr    
 
-def selectBooksForFirstReader(gamma_1, simulationParams, **kw):
+def selectBooksForFirstReader(gammas, simulationParams, **kw):
     '''
         selects a number of books, parametrized by lambda_1 ("interest in reading")
         returns dictionary: book_no => book_score (bookScores[j] = x_{1j})
@@ -18,12 +18,15 @@ def selectBooksForFirstReader(gamma_1, simulationParams, **kw):
     if 'poissonFunction' in kw: poisson = kw['poissonFunction']
     if 'sampleFrom15Function' in kw: sampleFrom15 = kw['sampleFrom15Function']
     
-    numBooks = poisson(expr.psiFunction(gamma_1, simulationParams), (1)) # TODO: verify (not sure what psi SUBSCRIPT GAMMA means here..)
+    numBooks = poisson(expr.psiFunction(gammas[0], simulationParams), (1)) # TODO: verify (not sure what psi SUBSCRIPT GAMMA means here..)
     bookScores = {}
     for j in range(numBooks):
-        bookScores[j] = sampleFrom15() # TODO: implement
+        bookScores[j] = sampleFrom15([gammas[0]], [], 0, simulationParams) # TODO: check
 
     return numBooks, bookScores
+
+def uScoreFromXScore(xScore):
+    return math.exp(-xScore)
 
 def selectBooksForIthReader(gammas, numBooks, prevBookScoreList, simulationParameters):
     tao = simulationParameters.tao
@@ -39,27 +42,28 @@ def selectBooksForIthReader(gammas, numBooks, prevBookScoreList, simulationParam
     bookScores = {}
     # consider previously read books:
     for j in range(numBooks):
-        gammaUSum = sum([gammas[k]*math.exp(-prevBookScoreList[k-1].get(j, 0.0)) for k in range(curReaderI)])
+        gammaUSum = sum([gammas[k]*uScoreFromXScore(prevBookScoreList[k-1].get(j, 0.0)) for k in range(curReaderI)])
         prob = 1.0 - (expr.kappaFunction(numTimesRead[j], tao + gammas[curReaderI] + gammaUSum, simulationParameters) \
                     / expr.kappaFunction(numTimesRead[j], tao + gammaUSum, simulationParameters))
         if prob.flipCoin(prob):
             bookScores[j] = 0.0
     
     # consider unread books:
-    numAdditionalBooks = poisson(expr.psiTildeFunction(gammas[curReaderI], sum([gammas[k] for k in range(curReaderI)]), simulationParameters), (1)) # TODO: implement
+    numAdditionalBooks = poisson(expr.psiTildeFunction(gammas[curReaderI], sum([gammas[k] for k in range(curReaderI)]), simulationParameters), (1)) # TODO: check
     for j in range(numBooks, numBooks+numAdditionalBooks):
         bookScores[j] = 0.0
     
     # assign scores
     for bookNo in bookScores:
-        bookScores[bookNo] = sampleFrom15() # TODO: implement
+        uList = [uScoreFromXScore(prevBookScoreList[k-1].get(j, 0.0)) for k in range(curReaderI)]
+        bookScores[bookNo] = sampleFrom15(gammas[:curReaderI+1], uList, numTimesRead[bookNo], simulationParameters) # TODO: check
     
     return numBooks + numAdditionalBooks, bookScores
 
 def generateBipartiteGraph(simulationParameters, gammas):
-    numBooks, firstScores = selectBooksForFirstReader(gammas[0], simulationParameters)
+    numBooks, firstScores = selectBooksForFirstReader(gammas, simulationParameters)
     allScores = [firstScores]
-    for i in range(len(gammas)-1):
+    for _ in range(len(gammas)-1):
         numBooks, scores = selectBooksForIthReader(gammas, numBooks, allScores, simulationParameters)
     sparseMatrix = []
     for scores in allScores:
--- bipartite-py/source/prob.py
+++ bipartite-py/source/prob.py
@@ -12,14 +12,14 @@ def flipCoin(p):
     '''
     return True if random.random() < p else False
 
-def sampleFrom15(j, gammas, uxjList, mj, parameters):
+def sampleFrom15(gammas, uxjList, mj, parameters):
     '''
         sample from distribution [Caron 2012, equation 15]
     '''
+    assert len(gammas) == len(uxjList)+1
     y = random.random()
     tao = parameters.tao
     sigma = parameters.sigma
-    assert len(gammas) == len(uxjList)+1
     sjn = sum([gammas[i]*uxjList[i] for i in range(len(gammas)-1)])
     if -0.00001 < mj - sigma < 0.00001:
         return ((tao+sjn)/gammas[-1])*((1.0+(gammas[-1]/(tao+sjn)))**y - 1.0)
