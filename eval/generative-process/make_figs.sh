d28514343b7df8f659a6155ca5e93af584479ebf
Matthias Sperber <matthias.sperber@gmail.com>
2013-11-20 11:34:51 +0900

    fixed the generative plots

--- eval/generative-process/make_figs.sh
+++ eval/generative-process/make_figs.sh
@@ -6,25 +6,25 @@ tau=1
 
 alpha=1
 sigma=0
-../../bipartite-py/generate.py -g $gamma -a 2 -t $tau -s 0.5 -n 30 | ../../bipartite-py/plot_bipartite.py -o fig.alpha=$alpha.sigma=$sigma.png
+../../bipartite-py/generate.py -g $gamma -a $alpha -t $tau -s $sigma -n 30 | ../../bipartite-py/plot_bipartite.py -o fig.alpha=$alpha.sigma=$sigma.png
 
 alpha=5
 sigma=0
-../../bipartite-py/generate.py -g $gamma -a 2 -t $tau -s 0.5 -n 30 | ../../bipartite-py/plot_bipartite.py -o fig.alpha=$alpha.sigma=$sigma.png
+../../bipartite-py/generate.py -g $gamma -a $alpha -t $tau -s $sigma -n 30 | ../../bipartite-py/plot_bipartite.py -o fig.alpha=$alpha.sigma=$sigma.png
 
 alpha=10
 sigma=0
-../../bipartite-py/generate.py -g $gamma -a 2 -t $tau -s 0.5 -n 30 | ../../bipartite-py/plot_bipartite.py -o fig.alpha=$alpha.sigma=$sigma.png
+../../bipartite-py/generate.py -g $gamma -a $alpha -t $tau -s $sigma -n 30 | ../../bipartite-py/plot_bipartite.py -o fig.alpha=$alpha.sigma=$sigma.png
 
 alpha=2
 sigma=0.1
-../../bipartite-py/generate.py -g $gamma -a 2 -t $tau -s 0.5 -n 30 | ../../bipartite-py/plot_bipartite.py -o fig.alpha=$alpha.sigma=$sigma.png
+../../bipartite-py/generate.py -g $gamma -a $alpha -t $tau -s $sigma -n 30 | ../../bipartite-py/plot_bipartite.py -o fig.alpha=$alpha.sigma=$sigma.png
 
 alpha=2
 sigma=0.5
-../../bipartite-py/generate.py -g $gamma -a 2 -t $tau -s 0.5 -n 30 | ../../bipartite-py/plot_bipartite.py -o fig.alpha=$alpha.sigma=$sigma.png
+../../bipartite-py/generate.py -g $gamma -a $alpha -t $tau -s $sigma -n 30 | ../../bipartite-py/plot_bipartite.py -o fig.alpha=$alpha.sigma=$sigma.png
 
 alpha=2
 sigma=0.9
-../../bipartite-py/generate.py -g $gamma -a 2 -t $tau -s 0.5 -n 30 | ../../bipartite-py/plot_bipartite.py -o fig.alpha=$alpha.sigma=$sigma.png
+../../bipartite-py/generate.py -g $gamma -a $alpha -t $tau -s $sigma -n 30 | ../../bipartite-py/plot_bipartite.py -o fig.alpha=$alpha.sigma=$sigma.png
 
