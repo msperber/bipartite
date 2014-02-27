60efb16188fa405e8929d8795f822ceb6edf8940
Matthias Sperber <matthias.sperber@gmail.com>
2014-02-27 20:41:55 +0900

    option to append log to file, rather than using stdout

--- bipartite-py/tune_ppl_powell.py
+++ bipartite-py/tune_ppl_powell.py
@@ -16,9 +16,10 @@ Options:
     --alphaTheta x           initial alphaTheta hyperparameter [default: 1.0]
     --numIterationsPerRun n  number of iterations per parameter combination[default: 50]
     --vocabSize n            max vocab size [default: 100]
-    --alphaF_range s         string that denotes tuning range: min_max_nsteps [default: 0.1_2.0_10]
-    --alphaTheta_range s     string that denotes tuning range: min_max_nsteps [default: 0.1_2.0_10]
+    --alphaF_range s         string that denotes tuning range: min_max_nsteps [default: 0.1_5.0_10]
+    --alphaTheta_range s     string that denotes tuning range: min_max_nsteps [default: 0.1_5.0_10]
     --numTuningIterations n  number of iterations in Powell's search [default: 10]
+    --log f                  log file in which to print output (instead of stdout)
     (as always: -h for help)
 """
 from source.prob import HyperParameters
@@ -51,7 +52,16 @@ def seedRandom():
     random.seed(13)
     numpy.random.seed(13)
 
-
+def customPrint(logF, *k):
+    string = " ".join([str(i) for i in k])
+    if logF is None:
+        print string
+    else:
+        f = open(logF, "a")
+        f.write(string + "\n")
+        f.flush()
+        f.close()
+    
 def main(argv=None):
     arguments = docopt.docopt(__doc__, options_first=True, argv=argv)
     corpusFileName = arguments['<corpusFile>']
@@ -68,6 +78,7 @@ def main(argv=None):
     maxVocabSize = int(arguments.get('--vocabSize'))
     alphaFRangeStr = arguments.get('--alphaF_range')
     alphaThetaRangeStr = arguments.get('--alphaTheta_range')
+    logF = arguments.get('--log', None)
     
     ###########################
     ## MAIN PROGRAM ###########
@@ -76,18 +87,18 @@ def main(argv=None):
     startTime = time.time()
     
     
-    print "LOADING CORPUS", corpusFileName
-    print "loading limited # docs:", maxNumDocs
+    customPrint(logF, "LOADING CORPUS", corpusFileName)
+    customPrint(logF, "loading limited # docs:", maxNumDocs)
     lowercase = True
-    print "lowercasing:", lowercase
+    customPrint(logF, "lowercasing:", lowercase)
     minTokenLen=0
-    print "removing tokens smaller than:", minTokenLen
+    customPrint(logF, "removing tokens smaller than:", minTokenLen)
     removeStopWords=True
-    print "removing stopwords:", removeStopWords
-    print "limit vocab size:", maxVocabSize
+    customPrint(logF, "removing stopwords:", removeStopWords)
+    customPrint(logF, "limit vocab size:", maxVocabSize)
     minNumTokens=10
-    print "remove tokens with frequency <", minNumTokens
-    print "loading.."
+    customPrint(logF, "remove tokens with frequency <", minNumTokens)
+    customPrint(logF, "loading..")
     textCorpus = document_data.DocumentCorpus.loadFromCorpusFile(\
                             corpusFile=corpusFileName, 
                             maxNumDocs=maxNumDocs, 
@@ -97,18 +108,18 @@ def main(argv=None):
                             maxVocabSize=maxVocabSize, 
                             minNumTokens=minNumTokens)
     trainCorpus, testCorpus = textCorpus.split()
-    print "done:"
-    print "loaded", len(textCorpus), "documents", "with a total of", textCorpus.getTotalNumWords(), "words (using half for testing)"
-    print "vocab size:", textCorpus.getVocabSize()
+    customPrint(logF, "done:")
+    customPrint(logF, "loaded", len(textCorpus), "documents", "with a total of", textCorpus.getTotalNumWords(), "words (using half for testing)")
+    customPrint(logF, "vocab size:", textCorpus.getVocabSize())
     
     numInitialTopics=10
-    print "# initial topics:", numInitialTopics
+    customPrint(logF, "# initial topics:", numInitialTopics)
     baseHyperParams = HyperParameters(alpha=alpha, sigma=sigma, tau=tau, alphaTheta=alphaTheta, 
                                           alphaF=alphaF, aGamma=1.0, bGamma=1.0)
-    print "(initial) hyperparameters:", baseHyperParams
+    customPrint(logF, "(initial) hyperparameters:", baseHyperParams)
     
-    print "# sampling iterations per parameter combination:", numIterationsPerRun
-    print "# iterations in Powell's search:", numTuningIterations
+    customPrint(logF, "# sampling iterations per parameter combination:", numIterationsPerRun)
+    customPrint(logF, "# iterations in Powell's search:", numTuningIterations)
      
     
     # determine choices for alphaF and alphaTheta
@@ -145,15 +156,16 @@ def main(argv=None):
                     bestParam = param
                     bestPpl = ppl
                     bestRet = ret
+            customPrint(logF, "currently best params:", bestParam.__str__())
             baseHyperParams = bestParam
-        print "best ppl after tuning iteration", (tuningIter+1), ":", bestPpl
+        customPrint(logF, "best ppl after tuning iteration", (tuningIter+1), ":", bestPpl)
     
-    print "best parameter combination found:", bestParam
-    print "  with a PPL of", bestPpl
-    print "  log likelihood was", bestRet["logLikelihood"]
+    customPrint(logF, "best parameter combination found:", bestParam)
+    customPrint(logF, "  with a PPL of", bestPpl)
+    customPrint(logF, "  log likelihood was", bestRet["logLikelihood"])
     
     elapsedTime = (time.time() - startTime)
-    print "elapsed time:", elapsedTime, "seconds"
+    customPrint(logF, "elapsed time:", elapsedTime, "seconds")
 
 if __name__ == "__main__":
     sys.exit(main())
